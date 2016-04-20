41ace1716b2a373ca405dfc88a55a412723ff3d8
Tristan Swedish <tristan.swedish@gmail.com>
2016-04-20 03:50:02 -0400

    fix demo to make more sense

--- runDemo.sh
+++ runDemo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "...computing images..."
-./src/build/FaceMatcher models/shape_predictor_68_face_landmarks.dat img/$1
-./src/build/faceswap img/$1
+./src/build/FaceMatcher models/shape_predictor_68_face_landmarks.dat $1
+./src/build/faceswap $1
 echo "...done..."
