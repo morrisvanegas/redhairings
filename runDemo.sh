829475fc7a0032044d3c636f8e820e4efb29465e
morrisvanegas <morris.vanegas@gmail.com>
2016-04-27 22:21:29 -0400

    Save test images to desktop

--- runDemo.sh
+++ runDemo.sh
@@ -3,15 +3,23 @@
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied, defaulting to test case"
-    img="img/testoutanna.jpg"
+    img="img/test.jpg"
   else
   	img=$1
 fi
 
 echo "...computing images..."
+while true 
+do
+cp ~/Desktop/test.jpg ~/Desktop/test1.jpg
+#sleep 1
+
 ./src/build/FaceMatcher models/shape_predictor_68_face_landmarks.dat $img
-./src/build/faceswap $img img/vic_crop.png src/ron_output
-echo "running brush filter"
-cd src
-./a10
+./src/build/faceswap $img img/donald_trump.jpg src/ron_output
+#./src/build/faceswap $img img/vic_crop.png src/ron_output
+#open src/ron_output/output.png
+#echo "running brush filter"
+#cd src
+#./a10
 echo "...done..."
+done
